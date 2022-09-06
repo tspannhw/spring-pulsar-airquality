1f93f86ad1def50c24731d6aa3853e7eb928a26e
Chris Bono <chris.bono@gmail.com>
2022-09-06 17:44:14 -0500

    1. Adjusted pom.xml to use snapshots and pulsar starter
    2. Use SpringBoot auto-configured WebClient.Builder
    https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#io.rest-client.webclient
    
    3. Move to type-safe configuration properties
    https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#features.external-config.typesafe-configuration-properties
    
    4. Simplify usage of auto-configured spring-pulsar components

---
+++ src/main/java/dev/datainmotion/airquality/service/AirQualityProperties.java
@@ -0,0 +1,44 @@
+package dev.datainmotion.airquality.service;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@ConfigurationProperties("airnowapi")
+public class AirQualityProperties {
+
+	private final String baseUrl;
+	private final String airqualityUri;
+	private final String apiKey;
+	private final List<String> zipCodes;
+
+	@ConstructorBinding
+	public AirQualityProperties(
+			@DefaultValue("http://localhost:${server.port}") String baseUrl,
+			String airqualityUri,
+			String apiKey,
+			@DefaultValue("08520") List<String> zipCodes) {
+		this.baseUrl = baseUrl;
+		this.airqualityUri = airqualityUri;
+		this.apiKey = apiKey;
+		this.zipCodes = zipCodes;
+	}
+
+	public String getBaseUrl() {
+		return baseUrl;
+	}
+
+	public String getAirqualityUri() {
+		return airqualityUri;
+	}
+
+	public String getApiKey() {
+		return apiKey;
+	}
+
+	public List<String> getZipCodes() {
+		return zipCodes;
+	}
+}
